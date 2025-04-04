2bd6f14b3a74cb3fc748e63ba8620a438d8930e6
leisingha <120764124+leisingha@users.noreply.github.com>
2025-04-04 17:25:58 -0400

    create Project class

--- src/project.js
+++ src/project.js
@@ -1,1 +1,28 @@
 //project.js
+
+class Project{
+    #id = null;
+    #todoLists = [];
+    
+    constructor(title){
+        this.title = title;
+    }
+
+    get id(){
+        return this.#id;
+    }
+    set id(val){
+        this.#id = val;
+    }
+
+    addTodoList(obj){
+        this.#todoLists.push(obj);
+    }
+    removeTodoList(id){
+        let index = this.#todoLists.filter( (todoList) => todoList.id == id);
+        this.#todoLists.splice(index,1);
+    }
+    get todoLists(){
+        return this.#todoLists;
+    }
+}
