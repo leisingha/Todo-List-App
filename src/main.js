c032408ec65c359b535159a872d029d6c3a2b467
leisingha <120764124+leisingha@users.noreply.github.com>
2025-04-06 20:26:16 -0400

    add html skeleton

--- src/main.js
+++ src/main.js
@@ -1,24 +1,26 @@
 //main.js
 
 import { AppController } from "./appController.js";
+import './style.css'
 
 localStorage.clear();
 
-const proj = AppController.createNewProject('Default');
-console.log(localStorage.getItem(localStorage.key(0)));
-AppController.printAllProjects();
+// const proj = AppController.createNewProject('Default');
+// console.log(localStorage.getItem(localStorage.key(0)));
+// AppController.printAllProjects();
 
-const todoList = AppController.createNewTodoList('To do List', 'Regular Description');
-AppController.addToProject(proj,todoList);
+// const todoList = AppController.createNewTodoList('To do List', 'Regular Description');
+// AppController.addToProject(proj,todoList);
 
-const todo = AppController.createNewTodo('example 1', 'Just Notes', '12/12/25', 'low'); 
-AppController.addToTodolist(todoList, todo);
+// const todo = AppController.createNewTodo('example 1', 'Just Notes', '12/12/25', 'low'); 
+// AppController.addToTodolist(todoList, todo);
 
-todo.toggleComplete();
-console.log(todo.isComplete())
+// todo.toggleComplete();
+// console.log(todo.isComplete())
 
-AppController.removeTodo(todoList, todo);
-AppController.removeTodoList(proj, todoList);
-AppController.removeProject(proj);
+// AppController.removeTodo(todoList, todo);
+// AppController.removeTodoList(proj, todoList);
+// AppController.removeProject(proj);
 
-console.log(proj)
+// console.log(proj)
+
